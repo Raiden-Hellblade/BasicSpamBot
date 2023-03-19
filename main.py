b43b89d44875394afc24163a118b3b4770f1bbec
Raiden-Hellblade <70145648+Raiden-Hellblade@users.noreply.github.com>
2023-03-19 14:24:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pyautogui, time, keyboard
 time.sleep(5)
 f = open("script.txt", 'r')
-for word in f:
+for line in f:
     if keyboard.is_pressed('ctrl'):
         break
-    pyautogui.typewrite(word)
+    pyautogui.typewrite(line)
     time.sleep(0.5)
     pyautogui.press("enter")
     
